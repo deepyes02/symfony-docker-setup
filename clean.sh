0d75e1bd39349efed329c0c64a31231589d4bf22
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2024-11-22 13:47:35 +0900

    Added .env.example by default

--- clean.sh
+++ clean.sh
@@ -1,5 +1,4 @@
 docker compose down
 rm -rf ./phpmyadmin/
 rm -rf var/
-rm -rf .env.example
 rm -rf server/
--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,2 @@
-docker compose down
-rm -rf ./phpmyadmin/
-rm -rf var/
-rm -rf .env.example
-rm -rf server/
-
-
-##build the project
-
-cp .env .env.example
-docker compose up --build -d
+./clean.sh
+./build.sh
